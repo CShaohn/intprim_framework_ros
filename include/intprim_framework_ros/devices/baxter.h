43850b010d181422e6631fe8954d8a0b7430e630
shaohn <254236639@qq.com>
2021-05-23 13:14:08 +0800

    modify baxter config

--- include/intprim_framework_ros/devices/baxter.h
+++ include/intprim_framework_ros/devices/baxter.h
@@ -47,7 +47,7 @@ class BaxterInterface : public RobotInterface               //BaxterInterface ç»
         ros::Publisher                              m_statePublisher;
 
         BaxterState                                 m_currentState;
-        sensor_msgs::JointState                     m_publishMessage;
+        baxter_core_msgs::JointCommand              m_publishMessage;
 
         unsigned int                                m_control_frequency;
         float                                       m_messageTime;
